2d077df78b803b7b35156cc1aa4ebbb5e60624c4
Noemi Nahomy <noemi_angles@riseup.net>
2020-07-23 09:09:05 -0400

    added  test for project search

--- tests/backend/integration/services/test_project_search_service.py
+++ tests/backend/integration/services/test_project_search_service.py
@@ -7,7 +7,7 @@ from backend.services.users.user_service import UserService
 from backend.models.postgis.project import ProjectInfo, Project
 from shapely.geometry import Polygon
 from unittest.mock import patch
-from backend.models.dtos.project_dto import ProjectSearchBBoxDTO
+from backend.models.dtos.project_dto import ProjectSearchBBoxDTO, ProjectSearchDTO
 from backend.models.postgis.user import User
 from tests.backend.helpers.test_helpers import get_canned_json
 
@@ -149,3 +149,34 @@ class TestProjectSearchService(unittest.TestCase):
 
         # assert
         self.assertAlmostEqual(expected, 28276407740.2797, places=3)
+
+    def test_project_search(self):
+        if self.skip_tests:
+            return
+        user = User(id=3488526)
+        search_dto = ProjectSearchDTO()
+        search_dto.preferred_locale = None
+        search_dto.mapper_level = "BEGINNER"
+        search_dto.mapping_types = None
+        search_dto.project_statuses = ["PUBLISHED"]
+        search_dto.organisation_name = None
+        search_dto.organisation_id = 34
+        search_dto.team_id = 1
+        search_dto.campaign = "DengueFeverHonduras"
+        search_dto.order_by = "priority"
+        search_dto.order_by_type = "DESC"
+        search_dto.country = "Honduras"
+        search_dto.page = 1
+        search_dto.text_search = "6158"
+        search_dto.mapping_editors = ["ID"]
+        search_dto.validation_editors = ["ID"]
+        search_dto.teams = None
+        search_dto.interests = None
+        search_dto.created_by = 378610
+        search_dto.mapped_by = None
+        search_dto.favorited_by = None
+        search_dto.managed_by = None
+        search_dto.omit_map_results = False
+
+        search_dto.validate()
+        self.assertIsNotNone(ProjectSearchService.search_projects(search_dto, user))
